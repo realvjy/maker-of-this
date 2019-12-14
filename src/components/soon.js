67902eab42afbea40f9d1deb8d2298ef2dcbda53
realvjy <realvjy@gmail.com>
2019-12-15 00:23:28 +0530

    soon

--- src/components/soon.js
+++ src/components/soon.js
@@ -13,7 +13,7 @@ const Soon = () => {
             <img src="/logo.svg" className="logo" />
           </Link>
           <h1>Maker of this!</h1>
-          <p>If you are curios to know who is the maker of your faviroute products check here</p>
+          <p>If you are curios to know who is the maker of your favourite products check here</p>
           <ul className="links links-share">
             <li>
               <a className="btn-tw" href="https://twitter.com/intent/tweet?text=Design%20awesome%20landing%20page,%20mockup%20and%20presentations%20with%20this%20open-source%20illlustrations%20library.%20%20https://illlustrations.co%20by%20@realvjy"><img src="/twitter.svg"/> Share on twitter</a>
